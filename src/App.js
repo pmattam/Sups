60330f5cc185b33f6effe50841c34d9e355683f5
Prathyusha Mattam <prathyusha.mattam@gmail.com>
2018-04-18 19:20:38 -0400

    Integrating with the server

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
-// import React, { Component } from 'react';
-import React from 'react';
+import React, { Component } from 'react';
+// import React from 'react';
 import Sups from './components/Sups';
-import sups from './lib/supsList';
+// import sups from './lib/supsList';
+import { goFetch } from './lib/helper-functions';
 // import AddSupInputForm from './components/AddSupInputForm';
 
-let App = () => 
-  <div>
-    <Sups 
-      sups={sups}
-    />
-  </div>
-  
+// let App = () => 
+//   <div>
+//     <Sups 
+//       sups={}
+//     />
+//   </div>
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sups: []
+    }
+  }
+
+  componentDidMount() {
+    goFetch()
+        .then(res => res.json())
+        .then((result) => {
+            console.log(result);
+            this.setState({
+              sups: result
+            })
+        })
+  }
+
+  render() {
+    let { sups } = this.state;
+    console.log(sups);
+    return(
+      <div>
+        <Sups 
+          sups={sups}
+        />
+      </div>
+    )
+  }
+}
+
 export default App;
 
 
